eb476c8280fd001ad7d8160d23a050e2a97f6218
Oliver Bigelow <oelucy@gmail.com>
2020-09-09 15:08:06 -0700

    finsihed dropdown menu

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,11 +1,13 @@
-$(document).on("click", ".type", function(event){
-    const charDetails = event.target.parentElement
-    console.log(charDetails)
-    const type = charDetails.getAttribute("data-type")
-    console.log(type)
+$(document).on("click", ".deleteBtn", function(event){
+   const id = event.target.getAttribute("data-id")
+    $.ajax({
+        url: "/api/characters/" + id,
+        method: "DELETE"
+    }).then(function(res){
+        location.reload();
+        console.log(res)
+    })
 
 })
 
-const type = require("./chooseCharacterImg")
 
-console.log(type)
