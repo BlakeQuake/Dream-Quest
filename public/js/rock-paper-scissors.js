3cde5013418d68addc3017d4c639227920920845
Aragon <jearagon777Gmail.com>
2020-09-10 00:34:10 -0700

    finshed html css java rps minigame

--- public/js/rock-paper-scissors.js
+++ public/js/rock-paper-scissors.js
@@ -1,12 +1,12 @@
-const game = () => {
-    let playerSocre = 0;
-    let compScore = 0;
+var game = () => {
+    var wins = 0;
+    var loses = 0;
 
 //start game
-    const startGame = () => {
-        let playBtn = document.querySelector(".intro button");
-        let introScreen = document.querySelector(".intro");
-        let match = document.querySelector(".match");
+    var startGame = () => {
+        var playBtn = document.querySelector(".intro button");
+        var introScreen = document.querySelector(".intro");
+        var match = document.querySelector(".match");
 
         playBtn.addEventListener('click', () => {
             introScreen.classList.add("fadeOut");
@@ -14,20 +14,91 @@ const game = () => {
         });
     };
     //play match
-    const playMatch = () => {
-        let options = document.querySelectorAll(".options button");
-        let playerHand =document.querySelectorAll(".player-hand");
-        let computerHand =document.querySelectorAll(".computer-hand");
+    var playMatch = () => {
+        var options = document.querySelectorAll(".options button");
+        var playerHand =document.querySelector(".player-hand");
+        var computerHand =document.querySelector(".computer-hand");
         //computer options
-        const computerOptions = ["rock", "paper", "scissors"];
-        const computerChoice = Math.floor(Math.random() * 3);
-        computerOptions[computerChoice]
+        var computerOptions = ["rock", "paper", "scissors"];
 
-    } 
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                //computer choice
+                var computerNumber = Math.floor(Math.random() * 3);
+                var computerChoice = computerOptions[computerNumber];
+                //compare hands
+                compareHands(this.textContent, computerChoice);
+
+
+                //update images
+                playerHand.src = `./RockPaperScissors-images/${this.textContent}.jpeg`;
+                computerHand.src = `./RockPaperScissors-images/${computerChoice}.jpeg`;
+            });
+        });
+    };
+
+    var updateScore = () => {
+        var playerSocre = document.querySelector(".player-score p");
+        var computerSocre = document.querySelector(".computer-score p");
+        playerSocre.textContent = wins;
+        computerSocre.textContent = loses;
+    }
+
+    var compareHands = (playerChoice, computerChoice) => {
+        var winner = document.querySelector(".winner");
+        if (playerChoice === computerChoice) {
+            winner.textContent = "tie";
+            return;
+        }
+        
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors'){
+                winner.textContent = "you win"
+                wins++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "you lose"
+                loses++;
+                updateScore();
+                return;
+            }
+        }
+
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'rock'){
+                winner.textContent = "you win"
+                wins++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "you lose"
+                loses++;
+                updateScore();
+                return;
+            }
+        }
+
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'paper'){
+                winner.textContent = "you win"
+                wins++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "you lose"
+                loses++;
+                updateScore();
+                return;
+            }
+        }
+    }
+
+
 
     //call all inner function
     startGame();
-    // updateScore();
+    playMatch();
 };
 
 
