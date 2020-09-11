d0ac81814000d636356356b88ec18c4dfdc0900c
Oliver Bigelow <oelucy@gmail.com>
2020-09-11 11:32:14 -0700

    added stuff from dustins computer

--- public/js/boss.js
+++ public/js/boss.js
@@ -5,7 +5,7 @@ function deleteChar(charId) {
         url: "/api/characters/" + charId,
         method: "DELETE"
     }).then(function(res){
-        console.log(res)
+        alert(res)
     })
 }
 function fightBoss(){
@@ -23,16 +23,17 @@ function fightBoss(){
         let bosshp = 200;
         let bossStr = 50
         function attack(){
-            console.log(`you attacked boss for ${strength} damage!`);
+            alert(`you attacked boss for ${strength} damage!`);
             bosshp -= strength
             hitpoints -= 50;
-            setTimeout(()=> console.log(`Boss attacked you for ${bossStr} damage! You have ${hitpoints} health left!`), 500)
+            setTimeout(()=> alert(`Boss attacked you for ${bossStr} damage! You have ${hitpoints} health left!`), 500)
         if (hitpoints <= 0){
-            console.log("You've been defeated")
+            alert("You've been defeated")
             deleteChar(id);
         }
         if (bosshp <= 0){
-            console.log("You have restored peace to the land, Well Done hero!!!")
+            alert("You have restored peace to the land, Well Done hero!!!")
+            return
         }
         }
         const attackBtn = document.createElement("button");
